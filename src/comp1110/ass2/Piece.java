6b4009c34b3ac1e35d87bc67d8f3a4d4826525c4
Xiao Cui <u7386319@anu.edu.au>
2021-09-01 23:35:28 +0800

    change expression of piece, and move some method to Board.java

--- src/comp1110/ass2/Piece.java
+++ src/comp1110/ass2/Piece.java
@@ -2,110 +2,107 @@ package comp1110.ass2;
 
 public class Piece {
 	/**
-	 * For each pieces, it will be stored by an int array represent for the unit vector in it
+	 * For each pieces, it will be stored by an int array representing for the unit vector in it
 	 *
 	 * Rules:
-	 * color  shape
- 	 * r   ✡ -- ✡           0，1，3
-	 *           \
-	 *       ✡ -- ✡
+	 * the star✡ with parentheses() is regarded as the center star, and other star are marked as
+	 * a unit vector(0~5, unit vectors has been introduced in Location.java)
 	 *
-	 * o   ✡ -- ✡           0.1
+	 *
+	 * color  shape           set           note
+ 	 * r   ✡ --(✡)           1，2，3
+	 *         / \
+	 *       ✡    ✡
+	 *
+	 * o   ✡ --(✡)           1, 3
 	 *           \
 	 *            ✡
 	 *
-	 * y    ✡    ✡ -- ✡     1,5,0
-	 *       \  /
+	 * y    ✡ --(✡)-- ✡     0, 2, 3
+	 *         /
 	 *        ✡
 	 *
-	 * g    ✡ -- ✡          0,1,1
-	 *            \
-	 *             ✡
+	 * g    ✡ --(✡)         1, 3            for this piece 'g', set[0] represents for 2 stars in the same direction:
+	 *            \                             one star is at the unit vector,
+	 *             ✡                            and the other star is at twice the unit vector
 	 *              \
 	 *               ✡
 	 *
-	 * b    ✡ -- ✡ -- ✡     0,0,2
-	 *               /
-	 *             ✡
-	 *
-	 * i    ✡ -- ✡ -- ✡     0,0
-	 *
-	 * p    ✡ -- ✡          0,1,2
+	 * b    ✡ --(✡)-- ✡     0, 1, 3
 	 *            \
 	 *             ✡
-	 *            /
+	 *
+	 * i    ✡ -- ✡ -- ✡     0, 3
+	 *
+	 * p    ✡    ✡          0, 1, 4, 5      for this piece 'p', the center star should not be placed on Board
+	 *       \  /
+	 *       ( )-- ✡
+	 *         \
 	 *          ✡
 	 *
-	 * use 0~5 stand for different orientations
-	 * all integers in shape[] plus orientation, the shape of piece will not change and get a new piece
-	 *
+	 * use 0~5 stands for different orientations
+	 * all integers in set[] plus orientation, the shape of piece will not change and get a new piece
 	 *
 	 */
+
 	private int[] shape;
 	private char color;
-	private int length;
 
 	/**
 	 * create integer arrays of different length when different color input,
 	 * and set shape[] by following rules above
-	 * @param color
+	 *
+	 * @param color should be one of those 7 char above
 	 */
 	Piece(char color){
-		switch (color){
-			case 'r':
-				this.length = 4;
-				this.shape = new int[this.length-1];
+		switch (color) {
+			case 'r' -> {
+				this.shape = new int[3];
+				this.shape[0] = 1;
+				this.shape[1] = 2;
+				this.shape[2] = 3;
+				this.color = color;
+			}
+			case 'o' -> {
+				this.shape = new int[2];
+				this.shape[0] = 1;
+				this.shape[1] = 3;
+				this.color = color;
+			}
+			case 'y' -> {
+				this.shape = new int[3];
+				this.shape[0] = 0;
+				this.shape[1] = 2;
+				this.shape[2] = 3;
+				this.color = color;
+			}
+			case 'g' -> {
+				this.shape = new int[2];
+				this.shape[0] = 1;
+				this.shape[1] = 3;
+				this.color = color;
+			}
+			case 'b' -> {
+				this.shape = new int[3];
 				this.shape[0] = 0;
 				this.shape[1] = 1;
 				this.shape[2] = 3;
-				this.color=color;
-				break;
-			case 'o':
-				this.length = 3;
-				this.shape = new int[this.length-1];
+				this.color = color;
+			}
+			case 'i' -> {
+				this.shape = new int[2];
+				this.shape[0] = 0;
+				this.shape[1] = 3;
+				this.color = color;
+			}
+			case 'p' -> {
+				this.shape = new int[4];
 				this.shape[0] = 0;
 				this.shape[1] = 1;
-				this.color=color;
-				break;
-			case 'y':
-				this.length = 4;
-				this.shape = new int[this.length-1];
-				this.shape[0] = 1;
-				this.shape[1] = 5;
-				this.shape[2] = 0;
-				this.color=color;
-				break;
-			case 'g':
-				this.length = 4;
-				this.shape = new int[this.length-1];
-				this.shape[0] = 0;
-				this.shape[1] = 1;
-				this.shape[2] = 1;
-				this.color=color;
-				break;
-			case 'b':
-				this.length = 4;
-				this.shape = new int[this.length-1];
-				this.shape[0] = 0;
-				this.shape[1] = 0;
-				this.shape[2] = 2;
-				this.color=color;
-				break;
-			case 'i':
-				this.length = 3;
-				this.shape = new int[this.length-1];
-				this.shape[0] = 0;
-				this.shape[1] = 0;
-				this.color=color;
-				break;
-			case 'p':
-				this.length = 4;
-				this.shape = new int[this.length-1];
-				this.shape[0] = 0;
-				this.shape[1] = 1;
-				this.shape[2] = 2;
-				this.color=color;
-				break;
+				this.shape[2] = 4;
+				this.shape[4] = 5;
+				this.color = color;
+			}
 		}
 	}
 
@@ -114,45 +111,10 @@ public class Piece {
 	 * @param rotation 0~5
 	 */
 	public void rotatePiece(int rotation){
-		for(int i = 0; i < this.length - 1; i++)
+		for(int i = 0; i < this.shape.length; i++)
 			shape[i]=(shape[i] + rotation) % 6;
 	}
 
-	/**
-	 *
-	 * @param board
-	 * @param loc the start location to place the piece
-	 * @return whether loc on board is valid to place the Piece
-	 */
-	public boolean isPieceValid(Board board,Location loc){
-		return true;//TODO: complete after Board
-	}
-
-	/**
-	 *
-	 * @param board
-	 * @param loc the start location to place the piece
-	 */
-	public void placePiece(Board board,Location loc){
-		//TODO: complete after Board
-	}
-
-	/**
-	 *
-	 * @param str format in readme.md
-	 */
-	public static void placePiece(Board board, String str){
-		//TODO:
-	}
-
-	/**
-	 * notice: it will also remove the wizard puzzle star
-	 * @param board
-	 * @param loc the start location to place the piece
-	 */
-	public void removePiece(Board board,Location loc){
-		//TODO: complete after Board
-	}
 
 	/**
 	 *
